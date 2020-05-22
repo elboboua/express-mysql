73a70344dd96d149bfad683e28e952c44d96f0c7
elboboua <44094673+elboboua@users.noreply.github.com>
2020-05-22 15:13:54 +0300

    added handlebars

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const hbs = require('express-handlebars');
 const knex = require('./knex/knex.js');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs( {
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname = "./views/pages"
+}))
+
+
+// app.get('/', (req, res) => {
+//   res.render('test.hbs');
+// }) 
+
 app.get('/names', async (req, res) => {
-  let names = await knex.select().from('test_table');
-  res.send(names);
+  let context= {}
+  let data = await knex.select().from('test_table');
+
+  context.names = JSON.parse(JSON.stringify(data));
+  
+  console.log(context);
+  res.render('names', context)
 });
 
 app.get('/addname/:name', async (req, res) => {
